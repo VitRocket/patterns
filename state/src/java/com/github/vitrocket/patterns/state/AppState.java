a85f966d9efe9a4fa131fcc309c5f25a3aef77de
VitRocket <VitRocket@gmail.com>
2018-03-26 23:45:33 +0300

    added pattern State

---
+++ state/src/java/com/github/vitrocket/patterns/state/AppState.java
@@ -0,0 +1,5 @@
+/**
+ * @author by Vitaliy on 26.03.2018.
+ */
+public class AppState {
+}
---
+++ state/src/java/com/github/vitrocket/patterns/state/State.java
@@ -0,0 +1,7 @@
+package com.github.vitrocket.patterns.state;
+
+/**
+ * @author by Vitaliy on 26.03.2018.
+ */
+public class State {
+}
