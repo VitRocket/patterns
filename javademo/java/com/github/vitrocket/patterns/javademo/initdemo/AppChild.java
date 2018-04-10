06956c4c40d0fd5a735f744a2d276597f277cdbc
VitRocket <vitrocket@gmail.com>
2018-04-10 23:24:53 +0300

    initialize demo

---
+++ javademo/java/com/github/vitrocket/patterns/javademo/initdemo/AppChild.java
@@ -0,0 +1,12 @@
+package com.github.vitrocket.patterns.javademo.initdemo;
+
+public class AppChild {
+
+    public static void main(String[] args) {
+        System.out.println("----->> Start program <<-----");
+        System.out.println("Hello!");
+
+        System.out.println("----->> Create Child object <<-----");
+        Child child = new Child();
+    }
+}
